b051dadd7de40732a28a13d7679ee9911828063b
Anders Steen <anders.steen98@gmail.com>
2021-07-21 19:26:03 +0200

    create adder template

---
+++ adder.js
@@ -0,0 +1,9 @@
+var recipePartSource = `
+<p>Receptdelar:</p>
+<div class="recipePart">
+<label>Receptdelsnamn:</label>
+<input name="recipePartName" type="text">
+<p>Ingredienser:</p>
+<div class="ingredientList"></div>
+</div>
+<button>Lägg till receptdel</button>`
